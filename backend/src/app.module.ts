7c7b4162572dd41feb8f11030ea78a39d266700c
HaSsan <hassanharga@gmail.com>
2025-02-21 21:43:15 +0200

    add configuration files and enhance app structure with Swagger and basic auth

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
+    // AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+// eslint-disable-next-line prettier/prettier
+export class AppModule { }
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,13 +1,73 @@
+import { INestApplication, Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as basicAuth from 'express-basic-auth';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.enableCors();
 
-  console.log(
-    `Application is running on: http://localhost:${process.env.PORT ?? 3000}`,
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
+  app.setGlobalPrefix('api');
+
+  const appConfig = app.get<ConfigService>(ConfigService);
+  const swaggerUsername = appConfig.get<string>('SWAGGER_USERNAME')!;
+  const swaggerPassword = appConfig.get<string>('SWAGGER_PASSWORD')!;
+  const port = appConfig.get<number>('PORT') || 3000;
+
+  // Swagger
+  SwaggerModule.setup(
+    'docs',
+    app,
+    swaggerSetup(app, swaggerUsername, swaggerPassword),
   );
+
+  await app.listen(port);
+  Logger.log(`🚀 Application is running on: http://localhost:${port}`);
 }
 
-void bootstrap();
+function swaggerSetup(
+  app: INestApplication,
+  swaggerUsername: string,
+  swaggerPassword: string,
+) {
+  // Secure the documentation page
+
+  app.use(
+    ['/docs', '/docs-json'],
+    basicAuth({
+      challenge: true,
+      users: {
+        [swaggerUsername]: swaggerPassword,
+      },
+    }),
+  );
+
+  // Configure Swagger
+  const config = new DocumentBuilder().setVersion('1.0.0').addBearerAuth(
+    {
+      type: 'http',
+      in: 'header',
+      scheme: 'bearer',
+      bearerFormat: 'JWT',
+      name: 'JWT',
+      description: 'Enter JWT token',
+    },
+    'JWT-auth',
+  );
+  config.setTitle(`App API (${process.env.NODE_ENV})`);
+  config.setDescription(
+    `App's API Documentation (${process.env.NODE_ENV} Environment)`,
+  );
+
+  return SwaggerModule.createDocument(app, config.build());
+}
+
+bootstrap().catch((err) => {
+  Logger.error('Error starting application', err);
+  process.exit(1);
+});
